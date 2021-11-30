3aa4cbeee414ed3907e3e72b01e28f8840faf792
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-11-30 01:01:22 -0300

    implement new middlewares on policies routes

--- routes/policies.js
+++ routes/policies.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const isLogged = require('../middlewares/isLogged');
+const isAdmin = require('../middlewares/isAdmin');
+
 const policiesController = require('../controllers/policiesController')
 
 
-router.get('/', policiesController.getPolicies)
 
-router.get('/:id', policiesController.getPolicy)
+router.get('/', isLogged, isAdmin, policiesController.getPolicies)
+
+router.get('/:id',isLogged, isAdmin, policiesController.getPolicy)
 
 
 module.exports = router;
