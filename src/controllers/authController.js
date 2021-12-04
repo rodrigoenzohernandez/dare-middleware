fde22328137ec2405284c70035ac29c62c9c213a
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-12-04 18:14:14 -0300

    endpoint to get insurance token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,21 @@
-const authController = {
-  login(req, res) {
-    try {
-      res.json('Endpoint /login - WIP');
-    } catch (error) {
-      console.log(error);
+const axios = require('axios');
+const { baseUrl } = require('../../config/config');
 
-      res.json(error);
+const authController = {
+  async login(req, res) {
+    try {
+      // TODO: validate user before getting insurance token
+      const body = {
+        client_id: req.body.client_id,
+        client_secret: req.body.client_secret,
+      };
+
+      const response = await axios.post(`${baseUrl}login`, body);
+
+      await res.send(response.data);
+    } catch (error) {
+      res.status(error.response.data.statusCode);
+      res.send(error.response.data);
     }
   },
 };
