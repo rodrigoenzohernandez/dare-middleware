e4305cd944ab11c016122aa4e5cb093c9f767608
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-12-23 00:53:42 -0300

    edit baseUrl

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseUrl: 'https://dare-nodejs-assessment.herokuapp.com/api/',
+  baseUrl: 'https://dare-nodejs-assessment.herokuapp.com/api',
   clientId: 'dare',
   clientSecret: 's3cr3t',
 
