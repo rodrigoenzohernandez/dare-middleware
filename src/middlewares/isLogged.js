8032d079c5b2fd11338829164bc83a0d37ab3f39
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2022-01-03 17:04:22 -0300

    delete unused middlewares

--- src/middlewares/isLogged.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-  req.session.user = 'temporalUser'; // TODO: Delete this line when the /login logic is implemented
-
-  if (req.session.user) next();
-  else {
-    res.statusCode = 401;
-    res.send('Unauthorized');
-  }
-};
