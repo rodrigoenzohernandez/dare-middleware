949ddc4e25e11c4632908d9d1ba9e484598f695a
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2022-01-04 17:00:56 -0300

    apply linter into inInvalidLimit.test.js file

--- tests/unit/isInvalidLimit.test.js
+++ tests/unit/isInvalidLimit.test.js
@@ -1,16 +1,16 @@
 const isInvalidLimit = require('../../src/functions/isInvalidLimit');
 
 describe('UNIT TEST: function isInvalidLimit', () => {
-  test('Test valid limit', async () => {
+  test('valid limit', async () => {
     expect(await isInvalidLimit(5)).toStrictEqual(false);
   });
-  test('Test invalid NaN limit', async () => {
+  test('invalid NaN limit', async () => {
     expect(await isInvalidLimit('invalid limit')).toStrictEqual(true);
   });
-  test('Test invalid negative limit', async () => {
+  test('invalid negative limit', async () => {
     expect(await isInvalidLimit(-20)).toStrictEqual(true);
   });
-  test('Test invalid undefined limit', async () => {
+  test('invalid undefined limit', async () => {
     expect(await isInvalidLimit()).toStrictEqual(true);
   });
 });
